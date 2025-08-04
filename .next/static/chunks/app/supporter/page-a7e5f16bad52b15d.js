b768f273dca51de6de861e48c7c1bff8b038f9b9
shuff57 <shuff57@gmail.com>
2025-08-03 19:33:28 -0700

    Add type definitions and validation checks for layout component in Next.js

--- .next/static/chunks/app/supporter/page-a7e5f16bad52b15d.js
+++
@@ -1,9 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[582],{9529:function(e,r,t){Promise.resolve().then(t.bind(t,2722))},2722:function(e,r,t){"use strict";t.r(r),t.d(r,{default:function(){return SupporterPage}});var n=t(7437),o=t(2265);function SupporterPage(){return(0,o.useEffect)(function(){window.location.href="/supporter"},[]),(0,n.jsx)("div",{className:"min-h-screen flex items-center justify-center bg-gray-50 dark:bg-gray-900",children:(0,n.jsxs)("div",{className:"text-center",children:[(0,n.jsx)("div",{className:"animate-spin rounded-full h-32 w-32 border-b-2 border-blue-600 mx-auto mb-4"}),(0,n.jsx)("h2",{className:"text-xl font-semibold text-gray-700 dark:text-gray-200",children:"Loading..."})]})})}},622:function(e,r,t){"use strict";/**
- * @license React
- * react-jsx-runtime.production.min.js
- *
- * Copyright (c) Meta Platforms, Inc. and affiliates.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */var n=t(2265),o=Symbol.for("react.element"),s=Symbol.for("react.fragment"),a=Object.prototype.hasOwnProperty,i=n.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED.ReactCurrentOwner,u={key:!0,ref:!0,__self:!0,__source:!0};function q(e,r,t){var n,s={},c=null,f=null;for(n in void 0!==t&&(c=""+t),void 0!==r.key&&(c=""+r.key),void 0!==r.ref&&(f=r.ref),r)a.call(r,n)&&!u.hasOwnProperty(n)&&(s[n]=r[n]);if(e&&e.defaultProps)for(n in r=e.defaultProps)void 0===s[n]&&(s[n]=r[n]);return{$$typeof:o,type:e,key:c,ref:f,props:s,_owner:i.current}}r.Fragment=s,r.jsx=q,r.jsxs=q},7437:function(e,r,t){"use strict";e.exports=t(622)}},function(e){e.O(0,[971,472,744],function(){return e(e.s=9529)}),_N_E=e.O()}]);
